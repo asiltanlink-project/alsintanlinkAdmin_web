7ae0b0b48233cc51576fe9da8728423473506387
Bento Putra Hermanto <elben9229@gmail.com>
2021-03-28 18:54:24 +0700

    update link production

--- src/pages/urlLink.js
+++ src/pages/urlLink.js
@@ -1,42 +1,51 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-redeclare */
 
+// url local efath
+var url_alsintanlinkLocal = 'https://alsintanlink.com';
+var url_alsintanlinkProduction =
+  'http://alsintanlink-api.litbang.pertanian.go.id/';
+
 //URL LOGIN
-var url_login = 'https://alsintanlink.com/api/admin/login';
+var url_login = url_alsintanlinkProduction + '/api/admin/login';
 
 // ShowTransaction
-var url_getProvince = 'https://alsintanlink.com/api/location/province';
-var url_getCity = 'https://alsintanlink.com/api/location/city';
-var url_getDistrict = 'https://alsintanlink.com/api/location/district';
-var url_getAllData = 'https://alsintanlink.com/api/admin/';
+var url_getProvince = url_alsintanlinkProduction + '/api/location/province';
+var url_getCity = url_alsintanlinkProduction + '/api/location/city';
+var url_getDistrict = url_alsintanlinkProduction + '/api/location/district';
+var url_getAllData = url_alsintanlinkProduction + '/api/admin/';
 var url_showDetailFarmer =
-  'https://alsintanlink.com/api/admin/show_detail_farmer';
-var url_showDetailUpja = 'https://alsintanlink.com/api/admin/show_detail_upja';
-var url_getDetailAlsin = 'https://alsintanlink.com/api/admin/show_detail_alsin';
+  url_alsintanlinkProduction + '/api/admin/show_detail_farmer';
+var url_showDetailUpja =
+  url_alsintanlinkProduction + '/api/admin/show_detail_upja';
+var url_getDetailAlsin =
+  url_alsintanlinkProduction + '/api/admin/show_detail_alsin';
 var url_getDetailAlsinItem =
-  'https://alsintanlink.com/api/admin/show_detail_alsin_item';
+  url_alsintanlinkProduction + '/api/admin/show_detail_alsin_item';
 var url_getDetailTransaction =
-  'https://alsintanlink.com/api/admin/show_detail_transaction';
+  url_alsintanlinkProduction + '/api/admin/show_detail_transaction';
 var url_getDetailTransactionItem =
-  'https://alsintanlink.com/api/admin/show_detail_transaction_alsin';
+  url_alsintanlinkProduction + '/api/admin/show_detail_transaction_alsin';
 var url_getAllUpjaTransaction =
-  'https://alsintanlink.com/api/admin/show_all_upja_traction';
+  url_alsintanlinkProduction + '/api/admin/show_all_upja_traction';
 var url_sendUpjaAlert = 'http://alsintanlink.com/api/admin/send_upja_alert';
 var url_showAllTransaction =
-  'https://alsintanlink.com/api/admin/show_all_transaction';
-var url_getVillage = 'https://alsintanlink.com/api/location/village';
-var url_allAlsinType = 'https://alsintanlink.com/api/upja/show_all_alsin_type';
+  url_alsintanlinkProduction + '/api/admin/show_all_transaction';
+var url_getVillage = url_alsintanlinkProduction + '/api/location/village';
+var url_allAlsinType =
+  url_alsintanlinkProduction + '/api/upja/show_all_alsin_type';
 var url_showSparePartType =
-  'https://alsintanlink.com/api/admin/show_spare_part_type?alsin_type_id=';
+  url_alsintanlinkProduction + '/api/admin/show_spare_part_type?alsin_type_id=';
 var url_showSparePart =
-  'https://alsintanlink.com/api/admin/show_spare_part?spare_part_type_id=';
+  url_alsintanlinkProduction + '/api/admin/show_spare_part?spare_part_type_id=';
 var url_deleteSparePart =
-  'https://alsintanlink.com/api/admin/delete_spare_part';
+  url_alsintanlinkProduction + '/api/admin/delete_spare_part';
 var url_updateSparePart =
-  'https://alsintanlink.com/api/admin/update_spare_part_type';
+  url_alsintanlinkProduction + '/api/admin/update_spare_part_type';
 
-var url_uploadExcel = 'https://alsintanlink.com/api/admin/import_spare_part';
-var url_manual_book = "http://alsintanlink.com/api/manual_book/admin"
+var url_uploadExcel =
+  url_alsintanlinkProduction + '/api/admin/import_spare_part';
+var url_manual_book = 'http://alsintanlink.com/api/manual_book/admin';
 
 export {
   url_login,
@@ -60,5 +69,5 @@ export {
   url_deleteSparePart,
   url_updateSparePart,
   url_uploadExcel,
-  url_manual_book
+  url_manual_book,
 };
